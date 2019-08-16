fa410584b9135b1adcf6c99736859ff562ded42b
hdavidalderete <50747492+hdavidalderete@users.noreply.github.com>
2019-08-16 16:57:22 -0300

    Add files via upload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+
+  userSub: Subscription;
+  isAuthenticated = false;
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(
+      user => {
+        this.isAuthenticated =  !!user; // !user ? false : true;
+      }
+    );
+  }
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+}
